e27a3bd08d57af1fbad2a226ad7a370d95681412
Ognyan Bankov <ogibankov@gmail.com>
2016-07-22 16:03:17 +0300

    replacing MyAppEvent with AppEvent

--- app/src/main/java/com/bolyartech/forge/skeleton/dagger/basic/app/MyAppEvent.java
+++
@@ -1,7 +0,0 @@
-package com.bolyartech.forge.skeleton.dagger.basic.app;
-
-/**
- * Created by ogre on 2015-11-17 08:53
- */
-public interface MyAppEvent {
-}
--- app/src/main/java/com/bolyartech/forge/skeleton/dagger/basic/units/main/Ev_LoginAbortRequestedEvent.java
+++ app/src/main/java/com/bolyartech/forge/skeleton/dagger/basic/units/main/Ev_LoginAbortRequestedEvent.java
@@ -1,10 +1,11 @@
 package com.bolyartech.forge.skeleton.dagger.basic.units.main;
 
-import com.bolyartech.forge.skeleton.dagger.basic.app.MyAppEvent;
+
+import com.bolyartech.forge.android.app_unit.AppEvent;
 
 
 /**
  * Created by ogre on 2015-10-07
  */
-class Ev_LoginAbortRequestedEvent implements MyAppEvent {
+class Ev_LoginAbortRequestedEvent implements AppEvent {
 }
