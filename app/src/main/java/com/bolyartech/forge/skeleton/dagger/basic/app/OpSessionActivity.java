8cb0c632638f253ff7260d706389df0fa36c5f0b
Ognyan Bankov <ogibankov@gmail.com>
2017-02-19 10:12:23 +0200

    bugfix: using Handle to post the event because runOnUiTHread is synchronous if the current thread is the UI

--- app/src/main/java/com/bolyartech/forge/skeleton/dagger/basic/app/OpSessionActivity.java
+++ app/src/main/java/com/bolyartech/forge/skeleton/dagger/basic/app/OpSessionActivity.java
@@ -1,6 +1,8 @@
 package com.bolyartech.forge.skeleton.dagger.basic.app;
 
 
+import android.os.Handler;
+
 import com.bolyartech.forge.android.app_unit.OperationResidentComponent;
 import com.bolyartech.forge.android.app_unit.ResidentComponent;
 
@@ -8,12 +10,13 @@ import com.bolyartech.forge.android.app_unit.ResidentComponent;
 abstract public class OpSessionActivity<T extends ResidentComponent> extends SessionActivity<T>
         implements OperationResidentComponent.Listener {
 
+    private final Handler mHandler = new Handler();
 
     protected abstract void handleState();
 
 
     @Override
     public void onResidentOperationStateChanged() {
-        runOnUiThread(this::handleState);
+        mHandler.post(this::handleState);
     }
 }
