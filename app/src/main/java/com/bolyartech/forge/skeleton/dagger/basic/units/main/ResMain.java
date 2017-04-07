8cedb3df332899c43671965e144241239f87fbfb
Ognyan Bankov <ogibankov@gmail.com>
2017-04-07 13:32:56 +0300

    cosmetics

--- app/src/main/java/com/bolyartech/forge/skeleton/dagger/basic/units/main/ResMain.java
+++ app/src/main/java/com/bolyartech/forge/skeleton/dagger/basic/units/main/ResMain.java
@@ -4,24 +4,9 @@ import com.bolyartech.forge.android.app_unit.MultiOperationResidentComponent;
 import com.bolyartech.forge.skeleton.dagger.basic.app.CurrentUser;
 
 
-/**
- * Created by ogre on 2015-10-05
- */
+
 public interface ResMain extends MultiOperationResidentComponent<ResMain.Operation> {
 
-
-    enum Operation {
-        AUTO_REGISTERING,
-        LOGIN,
-        FACEBOOK_LOGIN,
-        LOGOUT
-    }
-
-
-    enum AutoregisteringError {
-        FAILED,
-        UPGRADE_NEEDED
-    }
 
     void autoLoginIfNeeded();
 
@@ -36,4 +21,17 @@ public interface ResMain extends MultiOperationResidentComponent<ResMain.Operati
     ResMain.AutoregisteringError getAutoregisteringError();
 
     CurrentUser getCurrentUser();
+
+    enum Operation {
+        AUTO_REGISTERING,
+        LOGIN,
+        FACEBOOK_LOGIN,
+        LOGOUT
+    }
+
+
+    enum AutoregisteringError {
+        FAILED,
+        UPGRADE_NEEDED
+    }
 }
